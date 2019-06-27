6c697fbcb2e095e5b63563eeaf416c2d5a211bc7
IvanLuLyf <835498692@qq.com>
2019-06-28 00:12:06 +0800

    Hello World

---
+++ src/main/java/cc/bunnies/pocketsnacks/controller/IndexController.java
@@ -0,0 +1,14 @@
+package cc.bunnies.pocketsnacks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    @ResponseBody
+    String index() {
+        return "Hello World";
+    }
+}
