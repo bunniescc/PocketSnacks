d0135ab0465e932adb845b4801040c57f3503d2b
IvanLuLyf <835498692@qq.com>
2019-06-27 23:51:50 +0800

    Initial

---
+++ src/main/java/cc/bunnies/pocketsnacks/PocketSnacksApplication.java
@@ -0,0 +1,13 @@
+package cc.bunnies.pocketsnacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketSnacksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketSnacksApplication.class, args);
+    }
+
+}
