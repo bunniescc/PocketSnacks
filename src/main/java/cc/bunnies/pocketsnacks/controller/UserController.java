9d5cb83ac05ef38e9a31a47ffa473576a5ddabdc
IvanLuLyf <835498692@qq.com>
2019-07-02 23:53:59 +0800

    修改了后台管理用户的页面

--- src/main/java/cc/bunnies/pocketsnacks/controller/UserController.java
+++ src/main/java/cc/bunnies/pocketsnacks/controller/UserController.java
@@ -1,29 +1,33 @@
 package cc.bunnies.pocketsnacks.controller;
 
-import cc.bunnies.pocketsnacks.dao.UserDao;
 import cc.bunnies.pocketsnacks.model.User;
+import cc.bunnies.pocketsnacks.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 public class UserController {
 
-    private UserDao userDao;
+    private UserService userService;
 
     @Autowired
-    public UserController(UserDao userDao) {
-        this.userDao = userDao;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @RequestMapping("/admin/user/list")
-    public String userList() {
+    public String userList(
+            ModelMap modelMap,
+            @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(name = "size", required = false, defaultValue = "10") int size
+    ) {
+        List<User> users = userService.getUsersByPage(page, size);
+        modelMap.addAttribute("users", users);
         return "admin/user_list";
     }
 }
