3401aad929d0bceea539ad28d4fda850084af2ab
IvanLuLyf <835498692@qq.com>
2019-07-02 22:52:54 +0800

    添加后台第一个页面

--- src/main/java/cc/bunnies/pocketsnacks/controller/UserController.java
+++ src/main/java/cc/bunnies/pocketsnacks/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -21,22 +22,8 @@ public class UserController {
         this.userDao = userDao;
     }
 
-    @RequestMapping("/user/list")
-    public String list(ModelMap modelMap) {
-        List<User> users = userDao.getAllUser();  //获取所有用户
-        modelMap.addAttribute("users", users);  //将数据传递给view
-        return "user_list";  //view 的名字
-    }
-
-    @RequestMapping(value = "/user/add", method = RequestMethod.POST)
-    public String add(ModelMap modelMap, @RequestParam String username, @RequestParam String password) {
-        User user = User.builder().username(username).password(password).build();
-        int uid = userDao.insert(user);
-        if (uid <= 0) {
-            modelMap.addAttribute("err_msg", "插入出错");
-        }
-        List<User> users = userDao.getAllUser();
-        modelMap.addAttribute("users", users);
-        return "user_list";
+    @RequestMapping("/admin/user/list")
+    public String userList() {
+        return "admin/user_list";
     }
 }
